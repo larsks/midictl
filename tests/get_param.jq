5a6ec4d4117449dd856e659c9538a44a5c0527c6
Lars Kellogg-Stedman <lars@oddbit.com>
2026-03-12 10:12:38 -0400

    More robust mechanism for getting property values in test

---
+++ tests/get_param.jq
@@ -0,0 +1,29 @@
+# Searches Props array for a parameter by name
+# Usage: jq --arg param_name "volume" -f get_param.jq
+#
+# Searches both:
+# - Direct properties (e.g., "volume", "mute")
+# - Parameters in params array (e.g., "gain:Gain 1")
+
+def find_in_params($key):
+  .params as $arr |
+  if $arr then
+    # Iterate through params array by pairs (index 0, 2, 4, ...)
+    [range(0; $arr | length; 2)] |
+    map(. as $i | if $arr[$i] == $key then $arr[$i + 1] else empty end) |
+    first
+  else
+    null
+  end;
+
+# Search all Props elements
+[
+  .Props[] |
+  if has($param_name) then
+    .[$param_name]
+  else
+    find_in_params($param_name)
+  end
+] |
+map(select(. != null)) |
+first // null
--- tests/test_midictl.sh
+++ tests/test_midictl.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-check_param_val() {
-  local expected=$1
-  local have
-
-  have=$(pw-dump | jq -r '.[] | select(.info.props."node.name" == "gain.input").info.params.Props[1].params[1]')
-
-  echo "have $have, expected $expected"
-  [[ "$have" = "$expected" ]]
-}
-
 check_prop_val() {
-  local expected=$1
+  local param_name=$1
+  local expected=$2
   local have
 
-  have=$(pw-dump | jq -r '.[] | select(.info.props."node.name" == "gain.input").info.params.Props[0].volume')
+  have=$(pw-dump | jq -r '.[] | select(.info.props."node.name" == "gain.input").info.params' |
+    jq -r --arg param_name "$param_name" -f tests/get_param.jq)
 
   echo "have $have, expected $expected"
   [[ "$have" = "$expected" ]]
 }
 
 if ! [[ -x midictl ]]; then
-  echo "You need to build midictl first." >&2
+  echo "ERROR: you need to build midictl first." >&2
   exit 1
 fi
 
@@ -32,15 +24,21 @@ wpexec_pid=$!
 trap "kill $midictl_pid $wpexec_pid" exit
 
 # wait for midictl port to show up
+echo -n "waiting for midictl: "
 if ! timeout 10 sh -c 'while ! pw-link -io | grep -q midi-control:midi_in; do sleep 1; done'; then
+  echo failed
   echo "ERROR: timed out waiting for midictl port" >&2
   exit 1
 fi
+echo ok
 
+echo -n "waiting for gain plugin: "
 if ! timeout 10 sh -c 'while ! pw-link -io | grep -q gain.input; do sleep 1; done'; then
+  echo failed
   echo "ERROR: timed out waiting for gain.input port" >&2
   exit 1
 fi
+echo ok
 
 # link midi input to midictl
 pw-link 'Midi-Bridge:Midi Through Port-0 (capture)' 'midi-control:midi_in'
@@ -50,29 +48,36 @@ nodeid=$(pw-dump | jq -r '.[] | select(.info.props."node.name" == "gain.input").
 echo "found gain.input with id $nodeid"
 
 # reset test parameters
-pw-cli set-param "$nodeid" 2 '{"volume": 1.0}'
-pw-cli set-param "$nodeid" 2 '{"params": ["gain:Gain 1", 1.0]}'
+echo "resetting parameters to known values"
+pw-cli set-param "$nodeid" 2 '{"volume": 1.0}' >/dev/null
+pw-cli set-param "$nodeid" 2 '{"params": ["gain:Gain 1", 1.0]}' >/dev/null
 
-if ! check_param_val 1.000000; then
+echo -n "checking initial value of gain:Gain 1: "
+if ! check_prop_val 'gain:Gain 1' 1.000000; then
   echo "ERROR: failed to reset parameter value" >&2
   exit 1
 fi
 
-if ! check_prop_val 1.000000; then
+echo -n "checking initial value of volume: "
+if ! check_prop_val volume 1.000000; then
   echo "ERROR: failed to reset direct property value" >&2
   exit 1
 fi
 
+echo "sending midi cc to update gain"
 sendmidi dev 'Midi Through Port-0' cc 23 127
 sleep 2
-if ! check_param_val 10.000000; then
+echo -n "checking value of gain:Gain 1: "
+if ! check_prop_val 'gain:Gain 1' 10.000000; then
   echo "ERROR: failed to set parameter value" >&2
   exit 1
 fi
 
+echo "Sending midi cc to update volume"
 sendmidi dev 'Midi Through Port-0' cc 24 127
 sleep 2
-if ! check_prop_val 10.000000; then
+echo -n "checking value of volume: "
+if ! check_prop_val volume 10.000000; then
   echo "ERROR: failed to set direct property value" >&2
   exit 1
 fi
